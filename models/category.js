c8d60722590802c8532a8b48fd397b29133f695e
godzillalogan <kenlogan0117@gmail.com>
2021-07-22 14:26:05 +0800

    外,新增category種子資料

---
+++ models/category.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+//Schema，Schema是mongoose提供的定義資料結構的方式
+const categorySchema = new Schema({
+  categoryName:{
+    type: String,
+    required: true
+  },
+  categoryEnName:{
+    type: String,
+    required: true
+  },
+  categoryIcon:{
+    type: String,
+    required: true
+  },
+})
+
+
+module.exports = mongoose.model('Category', categorySchema)
--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,1 +1,41 @@
 db = require('../../config/mongoose')
+const Category = require('../category') //載入 record model
+
+const categorySeed = [
+  {
+    "categoryName": "家居物業",
+    "categoryEnName": "home",
+    "categoryIcon": "fas fa-home",
+  },
+  {
+    "categoryName": "交通出行",
+    "categoryEnName": "transportation",
+    "categoryIcon": "fas fa-shuttle-van",
+  },
+  {
+    "categoryName": "休閒娛樂",
+    "categoryEnName": "entertainment",
+    "categoryIcon": "fas fa-grin-beam",
+  },
+  {
+    "categoryName": "餐飲食品",
+    "categoryEnName": "food",
+    "categoryIcon": "fas fa-utensils",
+  },
+  {
+    "categoryName": "其他",
+    "categoryEnName": "other",
+    "categoryIcon": "fas fa-pen",
+  },
+]
+
+db.once('open', () => {
+  categorySeed.forEach(category => {
+    Category.create({
+      categoryName: category.categoryName,
+      categoryEnName: category.categoryEnName,
+      categoryIcon: category.categoryIcon,
+    });
+  })
+  console.log("done")
+})
