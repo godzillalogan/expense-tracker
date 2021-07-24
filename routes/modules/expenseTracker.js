1d821927f4a5e46f50c35b8e089dcc859f23d1eb
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-07-24 20:42:02 +0800

    家,增加delete功能

--- routes/modules/expenseTracker.js
+++ routes/modules/expenseTracker.js
@@ -42,6 +42,15 @@ router.post('/records/:id/edit', (req, res) => {
 })
 
 
+//delete
+router.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 
 module.exports = router
