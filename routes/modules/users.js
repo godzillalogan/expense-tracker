f85f0dbfbc5ddc3f25e1f0cfce188991ce6cfe57
godzillalogan <kenlogan0117@gmail.com>
2021-08-12 14:40:50 +0800

    U30 用 bcrypt 處理密碼：註冊篇

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,16 +1,14 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
-const passport = require('passport')
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
+router.post('/login', (req, res) => {
+})
 
 router.get('/register', (req, res) => {
   res.render('register')
@@ -19,22 +17,6 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
-  const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填。' })
-  }
-  if (password !== confirmPassword) {
-    errors.push({ message: '密碼與確認密碼不相符！' })
-  }
-  if (errors.length) {
-    return res.render('register', {
-      errors,
-      name,
-      email,
-      password,
-      confirmPassword
-    })
-  }
   // 檢查使用者是否已經註冊
   User.findOne({ email }).then(user => {
     // 如果已經註冊：退回原本畫面
@@ -46,23 +28,22 @@ router.post('/register', (req, res) => {
         password,
         confirmPassword
       })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      return User.create({
+    } 
+    // 如果還沒註冊：寫入資料庫
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash =>User.create({
         name,
         email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+        password:hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+    
   })
 })
 
-router.get('/logout', (req, res) => {
-  req.logout()
-  req.flash('success_msg', '你已經成功登出。')
-  res.redirect('/users/login')
-})
+
 
 module.exports = router
